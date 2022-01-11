1fd6e9d787e31d7427a5c2c05b02431932cf5367
Christian <pap3redits@gmail.com>
2022-01-11 13:10:49 -0500

    fixed typo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #sudo pacman -Syyu
 sudo pacman -S --needed - < pkglist.txt
 
-mkdir ~/Documents ~/Pictures ~/Downloads ~/Desktop ~/Videos ~/.config/alacritty
+mkdir  ~/Documents ~/Pictures ~/Downloads ~/Desktop ~/Videos ~/.config ~/.config/alacritty ~/.xmonad
 
 cp Space.png ~/Pictures/Space.png
 
@@ -14,3 +14,11 @@ cp -r xmonadfiles/scripts ~/.xmonad/scripts
 cp xmonadfiles/xmonad.hs ~/.xmonad/xmonad.hs
 
 cp alacritty ~/.config/alacritty/alacritty.yml
+
+cp -r fish ~/.config/
+chsh -s /bin/fish
+
+cd ~/repos
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
